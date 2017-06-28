280fd5ffa7a19e74f80c3bb07e6245d2b6a83015
Eric Roberts <eroberts@dealersocket.com>
2017-06-28 01:57:02 -0500

    add api key

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Beersite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.settings = config_for(:config)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
